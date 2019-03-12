fe635af496448ab4dd9ac43ebd6545d36485ee4c
Filipe Cabaco <filipecabaco@gmail.com>
2019-03-12 07:19:51 +0000

    Add typescript

--- src/container/App.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import styles from "./App.css";
-
-export const App = () => <div className={styles.container}>React Skeleton</div>;
--- tests/container/App.spec.tsx
+++ tests/container/App.spec.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { App } from "../../src/container/App";
 import { shallow } from "enzyme";
 describe("App", () => {
